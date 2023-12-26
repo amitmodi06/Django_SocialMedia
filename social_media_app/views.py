ed45fe3d6e8369a259a6351e5aa595e21abe2a42
amitmodi06 <amitmodi06@gmail.com>
2023-12-26 16:47:56 +0000

    Added logic for the post likes

--- social_media_app/views.py
+++ social_media_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 
 # Create your views here.
 @login_required(login_url='signin')
@@ -34,6 +34,28 @@ def upload(request):
         return redirect("/")
     else:
         return redirect("/")
+    
+
+@login_required(login_url='signin')
+def like_post(request, post_id):
+    username = request.user.username
+    post = Post.objects.get(id=post_id)
+
+    if post:
+        like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+        if like_filter == None:
+            new_like = LikePost.objects.create(post_id=post_id, username=username)
+            new_like.save()
+            post.no_of_likes = post.no_of_likes+1
+            post.save()
+            return redirect('index')
+        else:
+            like_filter.delete()
+            post.no_of_likes = post.no_of_likes-1
+            post.save()
+            return redirect('index')
+
 
 @login_required(login_url='signin')
 def settings(request):
